7293e8be2369926ded442d1d6915bda491b7e1cd
Your Name <shaktijat007@gmail.com>
2025-11-28 15:16:09 +0530

    fix:cart opration done

--- src/features/ShopCart/cartSlice.js
+++ src/features/ShopCart/cartSlice.js
@@ -14,11 +14,27 @@ const cartSlice = createSlice({
             if (existingItem) {
                 existingItem.quantity += 1;
             }else { 
-                action.payload.quantity = 1;
+                state.items.push({...action.payload, quantity: 1});
             }
-         state.items.push({...action.payload});
-        }
+         state.tempItem = [...state.items];
+         state.totalPrice = state.items.reduce((total, item) => total + item.price * item.quantity, 0);
+        },
+        removeFromCart: (state, action) => { 
+            state.items = state.items.filter(item => item.id !== action.payload.id);
+            state.tempItem = [...state.items];
+            state.totalPrice = state.items.reduce((total, item) => total + item.price * item.quantity, 0);
+        },
+        updateTempQuantity: (state, action) => {
+            const { id, quantity } = action.payload;
+            const itemToUpdate = state.items.find(item => item.id === id);  
+            if (itemToUpdate) {
+                itemToUpdate.quantity = quantity;
+            }
+            state.tempItem = [...state.items];
+            state.totalPrice = state.items.reduce((total, item) => total + item.price * item.quantity, 0);
+        }   
+
     },
 });
 export default cartSlice.reducer;
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, updateTempQuantity } = cartSlice.actions;
